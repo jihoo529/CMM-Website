ccd2fa862f2e9667737f77af148f43393994ddfd
krchris <u3572944@connect.hku.hk>
2024-02-21 14:24:43 +0800

    checking second commit

--- login.js
+++ login.js
@@ -1,54 +1,47 @@
 var express = require('express');
 var router = express.Router();
 const path = require('path');
-var pathname = path.join(__dirname,'../');
+var pathname = path.join(__dirname, '../');
 const { db } = require(pathname + "etc/mysql");
 const session = require('express-session');
 
-// Configure session middleware
-
-
 router.get('/', (req, res) => {
-    res.sendFile( pathname + "public/login.html" );
+  res.sendFile(pathname + "public/login.html");
 })
 
-router.post('/login', express.urlencoded({ extended: true }), (req, res) =>{
-    var response = "<p>"+req.body.loginName+"</p><p>"+req.body.loginPassword+"</p>";
-    var loginName = req.body.loginName;
-    var loginPW = req.body.loginPassword;
-    var query = `SELECT * FROM USERS WHERE NAME = ?`;
-    
-    db.query(query, [loginName], function(err, results){
-        if(err){
-            console.log(err);
-            return;
-        }
-        console.log(results[0])
-        console.log(results)
-        
-        if(results[0]){
-            if(results[0].PASSWORD == loginPW){
-                req.session.loginName = loginName;
-                req.session.userId = results[0].ID;
-                //res.sendFile(pathname + "public/index.html");
-                res.redirect(`/display/${loginName}`);
-                //res.send(response);
-                return;
-            }
-        }
-        res.sendFile(pathname + "public/login_fail.html");
-    });   
+router.post('/login', express.urlencoded({ extended: true }), (req, res) => {
+  var response = "<p>" + req.body.loginName + "</p><p>" + req.body.loginPassword + "</p>";
+  var loginName = req.body.loginName;
+  var loginPW = req.body.loginPassword;
+  var query = `SELECT * FROM USERS WHERE NAME = ?`;
+
+  db.query(query, [loginName], function (err, results) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(results[0])
+    console.log(results)
+
+    if (results[0]) {
+      if (results[0].PASSWORD == loginPW) {
+        req.session.loginName = loginName;
+        req.session.userId = results[0].ID;
+        //res.sendFile(pathname + "public/index.html");
+        res.redirect(`/display/${loginName}`);
+        //res.send(response);
+        return;
+      }
+    }
+    res.sendFile(pathname + "public/login_fail.html");
+  });
 })
 
 router.get('/logout', (req, res) => {
-    console.log("logout");
-    req.session.loginName = null;
-    req.session.userId = null;
-    //res.send("Logout successfully");
-    res.redirect('/');
+  console.log("logout");
+  req.session.loginName = null;
+  req.session.userId = null;
+  //res.send("Logout successfully");
+  res.redirect('/');
 })
-
-
 module.exports = router;
-
-
