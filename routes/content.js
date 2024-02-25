e30a5e0cf96975042223dd282ba6d22b8d5d5d03
jihoo529 <jihoolee529@gmail.com>
2024-02-25 22:47:37 +0800

    edited content.js

--- routes/content.js
+++ routes/content.js
@@ -7,24 +7,8 @@ const session = require('express-session');
 
 //write your APIs; upload-post, delete-post, edit-post
 router.post('/upload-post', express.urlencoded({ extended: true }), (req, res) => {
-    const content = req.body.content;
-    const escapedContent = content.replace(/'/g, "''"); //escapted ' => single quote using ''
-    
-    const query = `INSERT INTO contents (user_id, name, content) VALUES (${req.session.userId}, '${req.session.loginName}', '${escapedContent}')`;
-
-    db.query(query, function (err, result) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        else {
-            res.redirect('/display');
-        }
-    })
-    console.log(req.body.content);
+    //..................
 })
-
-
 
 //delete-post, edit-post...
 
