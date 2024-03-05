18089fb1aac24a1d5655a7727a16c78aec0c9535
suhan0906 <125735654+suhan0906@users.noreply.github.com>
2024-03-05 23:01:53 +0900

    edit content function

--- routes/content.js
+++ routes/content.js
@@ -5,11 +5,22 @@ var pathname = path.join(__dirname, '../');
 const { db } = require(pathname + "etc/mysql");
 const session = require('express-session');
 
-//write your APIs; upload-post, delete-post, edit-post
-router.post('/upload-post', express.urlencoded({ extended: true }), (req, res) => {
-    //..................
-})
 
-//delete-post, edit-post...
+router.put('/edit/:contentID', express.urlencoded({ extended: true }), (req, res) => {
+    var contentID = req.params.contentID;
+    var editedContent = req.body.content;
+    var escapedContent = editedContent.replace(/'/g, "''");
+
+    var query = `UPDATE contents SET content = '${escapedContent}' WHERE contentID = ${contentID}`;
+
+    db.query(query, function (err, result) {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error updating content");
+        } else {
+            res.status(200).send("Content updated successfully");
+        }
+    });
+});
 
 module.exports = router;
